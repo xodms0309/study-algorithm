df0a4b9c281aee8fb2cfc9fc8378c05c9c2e8921
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-03-17 00:58:08 +0900

    13회 이퍼 7번 난이도 중

---
+++ Epper/13thEpper7.cpp
@@ -0,0 +1,38 @@
+﻿/*
+이퍼 13회 7번
+배열을 회문으로 만드는 최소 수정 횟수 구하기
+*/
+#include <iostream>
+using namespace std;
+int solution(int* arr, int start, int end);
+int main() {
+	int num = 0;
+	int arr[100];
+	cin >> num; //원소 개수
+	for (int i = 0; i < num; i++) {
+		cin >> arr[i];
+	}
+	int start = 0, end = num-1;
+	cout << solution(arr, start, end);
+}
+int cnt=0;
+int solution(int* arr, int start, int end) {
+	while (start <= end) {
+		if (arr[start] == arr[end]) {
+			start += 1;
+			end -= 1;
+		}
+		else {
+			cnt++;
+			if (arr[start] < arr[end]) {
+				arr[start + 1] = arr[start] + arr[start + 1];
+				start += 1;
+			}
+			else {
+				arr[end - 1] = arr[end] + arr[end - 1];
+				end -= 1;
+			}
+		}
+	}
+	return cnt;
+}
