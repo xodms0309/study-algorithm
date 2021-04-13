ad5a042b38e6796fb7cf469897b1695c4f1e8bf4
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-04-13 22:59:38 +0900

    백준 15649 백트래킹 DFS

---
+++ 백준/15649.cpp
@@ -0,0 +1,32 @@
+/*
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+-1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+*/
+
+#include <iostream>
+using namespace std;
+int n, m;//n까지 숫자, 배열 길이는 m
+int depth;
+int arr[8] = { 0, };
+bool visited[8] = { false, };
+void dfs(int depth) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			arr[depth] = i+1;
+			dfs(depth + 1);
+			visited[i] = false;
+		}
+	}
+
+}
+int main() {
+	scanf_s("%d %d", &n, &m);
+	dfs(0);
+}
