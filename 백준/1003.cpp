11f69c0173541a0f2b43816758e21d3ca7896b88
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-04-19 23:44:08 +0900

    백준 1003:피보나치 수열 dinamic programming

---
+++ 백준/1003.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[41]={0,1,1};
+    for(int i=3;i<41;i++)   
+        arr[i]=arr[i-1]+arr[i-2];
+    int a;
+    cin>>a;
+ 
+    for(int i=0;i<a;i++){
+        int num;
+        cin>>num;
+        if(num==0)  cout<<"1 0\n";
+        else if(num==1) cout<<"0 1\n";
+        else cout<<arr[num-1]<<" "<<arr[num]<<'\n';
+    }
+}
