b97e47556ad625d1724eb6b8c4c2f9fea8ca9b1f
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-05-09 21:35:42 +0900

    Create K번째수.js

---
+++ Programmers/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    var answer = [];
+    var newArray=[];
+    for (let i=0; i<commands.length;i++){
+        newArray=array.slice(commands[i][0]-1, commands[i][1]).sort((a, b)=>a-b);
+        answer.push(newArray[commands[i][2]-1]);
+    }
+    return answer;
+}
