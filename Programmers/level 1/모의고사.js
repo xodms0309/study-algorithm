78ea1c1f4b49d1fde9f72802e3c5b63770f88284
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-06-23 13:32:57 +0900

    리팩토링🏗

--- Programmers/level 1/모의고사.js
+++ Programmers/level 1/모의고사.js
@@ -4,14 +4,18 @@ function solution(answers) {
     let secondArray=[2,1,2,3,2,4,2,5];
     let thirdArray=[3,3,1,1,2,2,4,4,5,5];
 
-    let first=answers.filter((elem, i)=>(elem===firstArray[i%firstArray.length])).length;
-    let second=answers.filter((elem, i)=>(elem===secondArray[i%secondArray.length])).length;
-    let third=answers.filter((elem, i)=>(elem===thirdArray[i%thirdArray.length])).length;
-
+    const correct=(answers, array)=>{
+        return answers.filter((elem, i)=>(elem===array[i%array.length])).length;
+    }
+    let first=correct(answers,firstArray);
+    let second=correct(answers,secondArray);
+    let third=correct(answers,thirdArray);
+    
     const max=Math.max(first, second, third);
+    
     if (max===first) answer.push(1);
     if (max===second) answer.push(2);
     if (max===third) answer.push(3);
-
+    
     return answer;
 }
