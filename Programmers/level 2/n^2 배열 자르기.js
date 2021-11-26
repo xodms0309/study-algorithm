023ae539621403f7367a4f1b69c9cb6d086fadfb
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-11-26 21:57:37 +0900

    Create n^2 배열 자르기.js

---
+++ Programmers/level 2/n^2 배열 자르기.js
@@ -0,0 +1,8 @@
+function solution(n, left, right) {
+    var answer = [];
+    while (left<=right){
+        answer.push(Math.max(Math.floor(left / n), left % n) + 1);
+        left++;
+    }
+    return answer;
+}
