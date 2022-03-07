879a8d43bc4987d622512b656a896f0073ffcbb7
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-03-07 17:26:12 +0900

    update: 백준 1026: 보물 (정렬)

--- 백준/1026.cpp
+++ 백준/1026.cpp
@@ -6,22 +6,20 @@ using namespace std;
 int main() {
 	int N;
 	cin >> N;
-	vector<int>A(N);
-	vector<int>B(N);
+	vector <int> A(N);
+	vector <int> B(N);
+	int answer = 0;
 	for (int i = 0; i < N; i++) {
-		scanf_s("%d", &A[i]);
+		cin >> A[i];
 	}
 	for (int i = 0; i < N; i++) {
-		scanf_s("%d", &B[i]);
-	}
+		cin >> B[i];
+	} 
 	sort(A.begin(), A.end());
-	int sum = 0;
-	while (B.size()) {
-		int max = *max_element(B.begin(), B.end());
-		int max_index= max_element(B.begin(), B.end())-B.begin();
-		sum += A.front() * max;
-		A.erase(A.begin());
-		B.erase(B.begin()+max_index);
+	sort(B.begin(), B.end());
+	reverse(B.begin(), B.end());
+	for (int i = 0; i < N; i++) {
+		answer += A[i] * B[i];
 	}
-	cout << sum;
+	cout << answer;
 }
