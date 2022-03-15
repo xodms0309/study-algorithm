cecfaa459a474f081308c41638b7024fc3f75aa7
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-03-15 21:41:42 +0900

    Update 2504.cpp

--- 백준/2504.cpp
+++ 백준/2504.cpp
@@ -1,55 +1,43 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <map>
 using namespace std;
 
 int main() {
 	string str;
 	stack <char> stack;
-	bool flag=true;
+	bool flag = true;
 	cin >> str;
 	int cnt = 1;
 	long long answer = 0;
+	map <char, char> bracket;
+	map <char, int> num;
+	bracket[')'] = '(';
+	bracket[']'] = '[';
+	num['('] = 2;
+	num['['] = 3;
 	for (int i = 0; i < str.length(); i++) {
-		if (str[i] == '(') {
-			stack.push('(');
-			cnt *= 2;
-		}
-		else if (str[i] == '[') {
-			stack.push('[');
-			cnt *= 3;
-		}
-		else if (str[i] == ')') {
-			if (stack.empty()) {
-				flag = false;
+		char op = str[i];
+		switch (op) {		
+			//여는 괄호는 스택에 넣음
+			case '(': case '[':
+				stack.push(op);
+				cnt *= num[op];
 				break;
-			}
-			else if (stack.top() == '(') {
-				if (str[i - 1] == '(') answer += cnt;
+			case ')': case ']':
+				if (stack.empty() || stack.top() != bracket[op]) {
+					cout << 0;
+					return 0;
+				}
+				if (str[i - 1] == bracket[op]) {
+					answer += cnt;
+				}
+				cnt /= num[bracket[op]];
 				stack.pop();
-				cnt /= 2;
-			}
-			else {
-				flag = false;
 				break;
-			}
-		}
-		else if (str[i] == ']') {
-			if (stack.empty()) {
-				flag = false;
-				break;
-			}
-			else if (stack.top() == '[') {
-				if (str[i - 1] == '[') answer += cnt;
-				stack.pop();
-				cnt /= 3;
-			}
-			else {
-				flag = false;
-				break;
-			}
 		}
 	}
-	if (!flag||!stack.empty()) cout << 0;
+	if (!stack.empty()) cout << 0;
 	else cout << answer;
 }
