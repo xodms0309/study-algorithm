0a48dba593466d2d548f1b34e6e408c0d328b71a
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-03-29 22:57:57 +0900

    백준 11053: 가장 긴 증가하는 부분 수열 (dp)

--- 백준/11053.cpp
+++ 백준/11053.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-using namespace std;	
-
+#include <algorithm>
+using namespace std;
 int main() {
 	int N;
 	cin >> N;
-	vector <int> arr(1001);
-	vector <int> DP(1001);
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
+	vector<int> dp(N+1,1);
+	vector<int> A(N+1);
 	int answer = 1;
-	DP[0] = 1;
-	for (int i = 1; i < N; i++) {
-		DP[i] = 1;
-		for (int j = 0; j < i; j++) {
-			if (arr[i] > arr[j]) {
-				DP[i] = max(DP[i], DP[j] + 1);
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 1; j < i; j++) {
+			if (A[i] > A[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
 			}
 		}
-		answer = max(answer, DP[i]);
+		answer = max(answer, dp[i]);
 	}
 	cout << answer;
 }
