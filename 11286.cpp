ae5335fb49dfa9efdfe2b6052d51133e70bfd1b7
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-04-05 22:47:28 +0900

    백준 11286: 절댓값 힙

---
+++ 11286.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef pair<int, int> pi;
+int main() {
+	int N;
+	cin >> N;
+	priority_queue<pi, vector<pi>, greater<pi>> pq;
+	while (N--) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (!pq.empty()) {
+				cout << pq.top().second << "\n";
+				pq.pop();
+			}
+			else cout << "0\n";
+		}
+		else {
+			pq.push({ abs(x), x });
+		}
+	}
+}
