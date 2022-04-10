0710cf38ec7294dba54b8389ab977ee4d36e5a7d
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-04-10 22:41:35 +0900

    백준 16401: 과자 나눠주기 (이분탐색)

---
+++ 백준/16401.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int check(int N, int mid, vector<int> &v) {		
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		cnt += v[i]/ mid;
+	}
+	return cnt;
+}
+int main() {
+	int M, N;
+	cin >> M >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	int left = 1;
+	int right = v[N-1];
+	int answer = 0;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		int cnt = check(N, mid, v);
+		if (cnt >= M) {
+			answer = mid;
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
+	}
+	cout << answer;
+
+}	
