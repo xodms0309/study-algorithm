495c1b8639272063dd8b4f3dc98bf0fe90e65ac3
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-08-30 16:27:29 +0900

    Update 문자열압축.js

--- Programmers/level 2/문자열압축.js
+++ Programmers/level 2/문자열압축.js
@@ -1,25 +1,25 @@
 function solution(s) {
-    let answer = s;
-    const slen=s.length;
-    //최대 압축할 수 있는 길이는 s의 길이의 절반
-    //i개 단위로 압축
-    for (let i=1; i<=slen/2; i++){
+    let answer = s.length;
+    const len = s.length;
+    
+    for (let i=1; i<=len-1; i++) {
         let cnt=1;
+        let cur=s.slice(0, i);
         let temp="";
-        for (let j=0; j<slen; j+=i){
-            //앞 뒤 단위로 자른 문자열 비교
-            let current=s.substring(j, j+i);
-            let next=s.substring(j+i, j+i+i);
-            if (current===next) cnt++;
+        for (let j=i; j<s.length; j+=i) {
+            let next=s.slice(j, j+i);
+            if (next==cur) cnt++;
             else {
-                if (cnt>1) temp=temp+cnt+current;
-                else {
-                    temp+=current;
-                }
+                if (cnt!==1) temp+=String(cnt);
+                temp+=cur; 
                 cnt=1;
+                cur=next;
             }
         }
-        if (answer.length>temp.length) answer=temp;
+        if (cnt!==1) temp+=String(cnt);
+        temp+=cur;
+        if (answer > temp.length) answer=temp.length;
     }
-    return answer.length;
+    
+    return answer;
 }
